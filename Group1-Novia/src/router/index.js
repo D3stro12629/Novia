c35a271bd093d7f0e5d787105b19d1f2c1da5fd2
D3stro12629 <zerwestin23@gmail.com>
2026-03-30 16:00:59 +0700

    1

--- Group1-Novia/src/router/index.js
+++ Group1-Novia/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import DashboardLayout from '../layouts/DashboardLayout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,61 @@ const router = createRouter({
       name: 'profile',
       component: () => import('../views/ProfileView.vue'),
     },
+    {
+      path: '/profilesettings',
+      name: 'profilesettings',
+      component: () => import('../views/ProfileSettings.vue'),
+    },
+    {
+      path: '/settings',
+      name: 'settings',
+      component: () => import('../views/SettingsView.vue'),
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: () => import('../views/ForbiddenView.vue'),
+    },
+    {
+      path: '/',
+      component: DashboardLayout,
+      children: [
+        {
+          path: '/settings',
+          component: () => import('../views/SettingsView.vue'),
+        }
+      ]
+    },  
+    //    {
+    //   path: "/",
+    //   component: DashboardLayout,
+    //   children: [
+    //     {
+    //       path: "",
+    //       name: "home",
+    //       component: HomeView,
+    //       // meta: {
+    //       //   title: "Dashboard",
+    //       // },
+    //     },
+    //     {
+    //       path: "category",
+    //       name: "category",
+    //       component: CategoryView,
+    //       // meta: {
+    //       //   title: "Category List",
+    //       // },
+    //     },
+    //     {
+    //       path: "profile",
+    //       name: "profile",
+    //       component: ProfileView,
+    //       // meta: {
+    //       //   title: "Profile View",
+    //       // },
+    //     },
+    //   ],
+    // },
   ],
 })
 
--- Group1-Novia/src/views/HomeView.vue
+++ Group1-Novia/src/views/HomeView.vue
@@ -1,9 +1,12 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="d-flex">
+        <Sidebar />
+        <div class="flex-grow-1">
+            <Navbar />
+            <div class="p-4">
+                <h1>Dashboard</h1>
+                <p>Welcome to the Article Dashboard! Here you can manage your articles, categories, and settings.</p>
+            </div>
+        </div>
+    </div>
 </template>
