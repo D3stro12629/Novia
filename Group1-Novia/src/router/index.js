235aee3433ebc95cb51a1156954dabc93a17248c
D3stro12629 <zerwestin23@gmail.com>
2026-04-06 14:58:11 +0700

    settings

--- Group1-Novia/src/router/index.js
+++ Group1-Novia/src/router/index.js
@@ -15,6 +15,11 @@ const router = createRouter({
       name: 'profile',
       component: () => import('../views/ProfileView.vue'),
     },
+    {
+      path: '/settings',
+      name: 'settings',
+      component: () => import('../views/SettingsView.vue'),
+    },
   ],
 });
 
